4fe2e34a8fdf659720132755b83e8d6f39029653
kimdaehun <dae9341@gmail.com>
2022-06-02 16:02:12 +0900

    firebase 호스팅

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave : false //vue에서 component명을 만들 때 반드시 단어의 조합(CmmMenu)으로 지어야 한다내용을 무시한다. 
+  lintOnSave : false, //vue에서 component명을 만들 때 반드시 단어의 조합(CmmMenu)으로 지어야 한다내용을 무시한다. 
+  outputDir: './dist'
 })
